611d7a1eece0f577d27f74bdadd6dbb0056d13ef
Jose Collas <joseherminiocollas@goatstone.com>
2018-09-13 17:06:36 -0700

    use tags in BarChart, add XAxis

---
+++ src/component/BarChart/XAxis.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as d3 from 'd3'
+import xScaleBarChart from './x-scale'
+
+const XAxis = ({data, width, height}) => {
+    const xAxisD3Node = d3.axisBottom()
+        .scale(xScaleBarChart(data, width))
+        .ticks(data.length / 2)
+
+    return (< g
+        ref={node => d3.select(node).call(xAxisD3Node)}
+        style={{
+            transform: `translateY(${height - 110}px)`,
+        }}
+        />)
+}
+export default XAxis
--- src/component/BarChart/index.js
+++ src/component/BarChart/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import * as d3 from 'd3'
 import Bars from './Bars'
 import ChartFrame from '../ChartFrame'
-import xScaleBarChart from './x-scale'
-// import yScale from './y-scale'
+import XAxis from './XAxis'
 
 const BarChart = ({ data = [], config }) => {
     const width = config.width
@@ -12,10 +10,6 @@ const BarChart = ({ data = [], config }) => {
     const barHeightMax = height - 120
     const background = config.background
     const margin = config.margin
-    
-    const xAxis = d3.axisBottom()
-        .scale(xScaleBarChart(data, width))
-        .ticks(data.length / 2)
 
     return (
         <ChartFrame
@@ -27,13 +21,11 @@ const BarChart = ({ data = [], config }) => {
             margin={margin}
             containerWidth={containerWidth}
             >
-            <g
-                ref={node => d3.select(node).call(xAxis)}
-                style={{
-                    transform: `translateY(${config.height - 110}px)`,
-                }}
-            />
-
+            <XAxis
+                data={data}
+                width={500}
+                height={300}
+                />
             <Bars
                 data={data}
                 width={width}
