8e97f8cd7bfbdea0f2b399880225fc98f44783e3
Jose Collas <joseherminiocollas@goatstone.com>
2019-07-03 07:20:56 -0700

    fix test

--- src/test/component.d3-react.test.js
+++ src/test/component.d3-react.test.js
@@ -10,23 +10,21 @@ Enzyme.configure({ adapter: new Adapter() })
 
 describe('D3 Framework', () => {
     it('renders without crashing', () => {
-        const div = document.createElement('div')
-        ReactDOM.render(<D3React
+        const mounted = Enzyme.mount(<D3React
             data
-            />, div)
-        ReactDOM.unmountComponentAtNode(div)
+        />)
     })
     it('has certain DOM elements', () => {
         const mounted = Enzyme.mount(<D3React
             data
-            />)
+        />)
         expect(mounted.find('section[data-id="container"]').length).toBe(1)
         expect(mounted.find('button[name="info"]').length).toBe(1)
     })
     it('shows the Info element', () => {
         const component = renderer.create(<D3React
             data
-            />)
+        />)
         const tree = component.toJSON()
         expect(tree).toMatchSnapshot()
     })
