b37435fde893d26295b211816e05280bf7e7f768
Jose Collas <joseherminiocollas@goatstone.com>
2019-07-05 11:56:38 -0700

    update eslint to consider .ts files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import D3React, { ContextA, defaultState } from './d3-react'
+import state from './state'
 
 ReactDOM.render(<ContextA.Provider value={defaultState}>
+    <div>
+        abc
+        {state.isInfoVisible ? 'a' : 'b'}
+    </div>
     <D3React />
 </ContextA.Provider>, document.getElementById('root'))
