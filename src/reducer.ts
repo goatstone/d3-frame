f04fcc1964c9657bd7350135465ba1ef095211b8
Jose Collas <joseherminiocollas@goatstonc.com>
2019-07-06 10:12:05 -0700

    update reducer

--- src/reducer.ts
+++ src/reducer.ts
@@ -4,6 +4,7 @@ import { StateInterface } from './state'
 enum actionTypes {
   SHOW_INFO = 'SHOW_INFO',
   HIDE_INFO = 'HIDE_INFO',
+  SET_CHART_TYPE = 'SET_CHART_TYPE'
 }
 export { actionTypes }
 
@@ -13,9 +14,14 @@ export interface ShowInfoInterface {
 export interface HideInfoInterface {
   type: typeof actionTypes.HIDE_INFO
 }
+export interface SetChartTypeInterface {
+  type: actionTypes.SET_CHART_TYPE
+  chartType: string;
+}
 export type AppActionsUnionInterface =
   | ShowInfoInterface
   | HideInfoInterface
+  | SetChartTypeInterface
 
 export interface dispatchInterface {
   (arg0: AppActionsUnionInterface): void
@@ -26,6 +32,8 @@ function reducer(state: StateInterface, action: AppActionsUnionInterface): State
       return Object.assign({}, state, { isInfoVisible: true })
     case actionTypes.HIDE_INFO:
       return Object.assign({}, state, { isInfoVisible: false })
+    case actionTypes.SET_CHART_TYPE:
+      return Object.assign({}, state, { chartType: action.chartType })
     default:
       throw new Error('Error: Action type does not exist')
   }
