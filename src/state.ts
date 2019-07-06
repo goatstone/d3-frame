66433ca47e92380ca37f950ed6bc45cf59d54e72
Jose Collas <joseherminiocollas@goatstonc.com>
2019-07-06 09:54:32 -0700

    add chart type props to state

--- src/state.ts
+++ src/state.ts
@@ -1,12 +1,21 @@
 import data from './data'
 
+const chartTypes = {
+  BAR: 'BAR',
+  LINE: 'LINE',
+  PIE: 'PIE',
+}
 export interface StateInterface {
   isInfoVisible: boolean;
   data: object;
+  chartTypes: object;
+  chartType: string;
 }
 const defaultState = {
   isInfoVisible: true,
   data,
+  chartTypes,
+  chartType: chartTypes.BAR,
 }
 
 export default defaultState
