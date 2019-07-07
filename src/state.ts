b7a30f7423616b7ac19bc9b08cd44ad24b88b6c9
Jose Collas <joseherminiocollas@goatstonc.com>
2019-07-06 17:06:16 -0700

    add state for symbol type

--- src/state.ts
+++ src/state.ts
@@ -5,17 +5,26 @@ const chartTypes = {
   LINE: 'LINE',
   PIE: 'PIE',
 }
+const symbolTypes = {
+  CIRCLE: 'CIRCLE',
+  SQUARE: 'SQUARE',
+  TRIANGLE: 'TRIANGLE',
+}
 export interface StateInterface {
   isInfoVisible: boolean;
   data: object;
   chartTypes: object;
   chartType: string;
+  symbolTypes: object;
+  symbolType: string;
 }
 const defaultState = {
   isInfoVisible: true,
   data,
   chartTypes,
   chartType: chartTypes.BAR,
+  symbolTypes,
+  symbolType: symbolTypes.CIRCLE,
 }
 
 export default defaultState
