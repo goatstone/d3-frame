eb4b76397da9d63a4eb73df5855a2188d5cf40ce
Jose Collas <joseherminiocollas@goatstonc.com>
2019-07-15 12:28:03 -0700

    add material icon to chart

--- src/component/ForceLayoutChart/index.js
+++ src/component/ForceLayoutChart/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { StoreContext } from '../../StoreContext'
 import ChartFrame from '../ChartFrame'
 
+const iconStyle = {
+  fill: 'red',
+  stroke: 'white',
+  transform: 'translate(30px, 130px) scale(3)',
+}
+
 function ForceLayoutChart() {
   return (
     <StoreContext.Consumer>
@@ -9,8 +15,13 @@ function ForceLayoutChart() {
         return (
           <div>
             <ChartFrame>
-              <text>
-                XXXXXXXXX
+              <text
+                className="material-icons"
+                style={iconStyle}
+              >
+                face
+              </text>
+              <text style={{ transform: 'translate(5px, 5px)' }} stroke="red" fill="red">
                 {state.chartType}
               </text>
             </ChartFrame>
