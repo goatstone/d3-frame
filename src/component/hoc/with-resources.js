6996121c248f8c560a7f426115f3529406906d02
Jose Collas <joseherminiocollas@goatstonc.com>
2019-07-15 09:58:23 -0700

    remove unused resource

--- src/component/hoc/with-resources.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const withResources = (Component, resources) => {
-    return (props) => {
-        return <Component {...resources} {...props} />
-    }
-}
-
-export default withResources
