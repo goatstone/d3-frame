dad885839aa1a9dd13a5b685b0ecbbfadcf3f619
Jose Collas <joseherminiocollas@goatstonc.com>
2019-07-15 23:15:43 -0700

    set up effect with simulation

--- src/component/ForceLayoutChart/index.js
+++ src/component/ForceLayoutChart/index.js
@@ -1,5 +1,10 @@
-import React, { useEffect, useState } from 'react'
-// import { forceSimulation, forceManyBody, forceCenter } from 'd3-force'
+import React, {
+  useEffect,
+  useState,
+  useContext,
+  useRef,
+} from 'react'
+import { forceSimulation, forceManyBody, forceCenter } from 'd3-force'
 import { StoreContext } from '../../StoreContext'
 import ChartFrame from '../ChartFrame'
 
@@ -8,14 +13,14 @@ const iconStyle = {
   stroke: 'white',
   transform: 'translate(30px, 130px) scale(3)',
 }
-// const width = 300
-// const height = 300
-// const nodes = [
-//   { text: 'abc' },
-//   { text: 'def' },
-//   { text: 'ghi' },
-//   { text: 'jkl' },
-// ]
+const width = 300
+const height = 300
+const nodes = [
+  { text: 'abc' },
+  { text: 'def' },
+  { text: 'ghi' },
+  { text: 'jkl' },
+]
 // let count = 0
 // function ticked() {
 //   // on tick
@@ -32,26 +37,72 @@ const iconStyle = {
 //   .force('charge', forceManyBody())
 //   .force('center', forceCenter(width / 2, height / 2))
 //   .on('tick', ticked)
-
 // console.log(simulation)
-
 function ForceLayoutChart() {
-  // const { state: stateC, actions } = useContext(StoreContext)
+  const { state: stateC } = useContext(StoreContext)
+  console.log('sss', stateC.iconNodes.nodes)
   const [count, setCount] = useState(0)
-  useEffect((eff) => {
-    console.log('effect', eff)
-    console.log('effect', count, setCount)
-    return () => {
-      setCount(count + 1)
-      // actions.setIsDrawn(true)
-    }
-  })
+  const latestCount = useRef(count)
 
+  // const elRef = React.createRef()
+  const elRef = React.useRef(null)
+  // let cancelT
+  // start on load
+  // function tick() {
+  //   console.log('tick', count)
+  //   //   // latestCount.current = count
+  //   cancelT = setInterval(() => {
+  //     setCount(latestCount.current + 1)
+  //     elRef.current.innerHTML = new Date().getSeconds()
+  //     // elRef.current.innerHTML = new Date()
+  //     console.log('xxx', count, elRef)
+  //   }, 1000)
+  //   setTimeout(() => clearInterval(cancelT), 12000)
+  // }
+  function ticked() {
+    console.log(nodes[0])
+    console.log(elRef)
+    elRef.current.style.top = `${nodes[0].x}px`
+    elRef.current.style.left = `${nodes[0].y}px`
+  }
+  useEffect(() => {
+    latestCount.current = count
+    // Read the mutable latest value
+    console.log(`You clicked ${latestCount.current} times`)
+    // const latestCount = useRef(count);
+    // tick()
+    const simulation = forceSimulation(nodes)
+      .force('charge', forceManyBody())
+      .force('center', forceCenter(width / 2, height / 2))
+
+    simulation.on('tick', () => ticked(elRef))
+
+    console.log('elRef:', elRef)
+    elRef.current.style.background = 'green'
+    elRef.current.style.position = 'absolute'
+    elRef.current.style.top = '20px'
+  }, [])
   return (
     <StoreContext.Consumer>
       {({ state }) => {
         return (
           <div>
+            <p>
+              You clicked
+              {count}
+              time
+            </p>
+            <button
+              type="button"
+              onClick={() => setCount(count + 1)}
+            >
+              Click me
+            </button>
+            <div
+              ref={elRef}
+            >
+              ssss
+            </div>
             {count}
             <ChartFrame>
               {state.iconNodes.nodes.map(n => {
