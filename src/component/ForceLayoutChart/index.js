d56746aa456442da9325302731c3c6e69498d412
Jose Collas <joseherminiocollas@goatstonc.com>
2019-07-16 13:11:07 -0700

    addjust parameters to find a good view of the icons

--- src/component/ForceLayoutChart/index.js
+++ src/component/ForceLayoutChart/index.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useContext } from 'react'
 import { forceSimulation, forceManyBody, forceCenter } from 'd3-force'
 import { StoreContext } from '../../StoreContext'
-import ChartFrame from '../ChartFrame'
+// import ChartFrame from '../ChartFrame'
 import getStyle, { styleTypes } from '../../get-style'
 
 let iconNodes = []
+const iconMax = 100
 function ForceLayoutChart() {
   const { state } = useContext(StoreContext)
   const svgGroupRef = React.useRef(null)
   const bounds = {
-    top: -20,
-    right: 600,
-    bottom: 250,
-    left: -40,
+    top: -140,
+    right: 900,
+    bottom: 500,
+    left: 0,
   }
   function ticked() {
     for (let i = 0; i < iconNodes.length; i += 1) {
@@ -22,28 +23,43 @@ function ForceLayoutChart() {
         && iconNodes[i].y < bounds.bottom
         && iconNodes[i].y > bounds.top
       ) {
-        svgGroupRef.current.children[i].style.transform = `translate(${iconNodes[i].x}px, ${iconNodes[i].y}px)`
+        svgGroupRef.current.children[i].style.transform = `
+        translate(${iconNodes[i].x}px, ${iconNodes[i].y}px)
+        `
       }
     }
   }
   useEffect(() => {
-    iconNodes = [...state.iconNodes]
+    iconNodes = [...state.iconNodes.slice(0, iconMax)]
     const simulation = forceSimulation(iconNodes)
-      .force('charge', forceManyBody())
-      .force('center', forceCenter(0, 75))
+      .force('charge', forceManyBody().distanceMax(50))
+      .force('center', forceCenter(250, 250))
+    simulation.tick([1])
     simulation.on('tick', () => ticked())
   }, [])
   const iconStyle = {
-    fontSize: '30px',
+    fontSize: '20px',
     fill: getStyle(state.theme, styleTypes.GENERIC).background(true),
-    stroke: 'darkgray',
+    stroke: 'gray',
   }
   return (
-    <ChartFrame>
+    <svg
+      className="main"
+      width="500"
+      height="500"
+      data-component-type="container"
+    >
+      <rect
+        fill="black"
+        width="100%"
+        height="500"
+      />
       <g ref={svgGroupRef} className={getStyle(state.theme, styleTypes.GENERIC).main()}>
-        {state.iconNodes.map(n => {
+        {state.iconNodes.slice(0, iconMax).map(n => {
           return (
             <text
+              x="0"
+              y="0"
               style={iconStyle}
               className="material-icons"
             >
@@ -52,7 +68,7 @@ function ForceLayoutChart() {
           )
         })}
       </g>
-    </ChartFrame>
+    </svg>
   )
 }
 export default ForceLayoutChart
