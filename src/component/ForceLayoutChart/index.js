d01beb5310e1d94a58d10bd6ff26df566df17b3b
Jose Collas <joseherminiocollas@goatstonc.com>
2019-07-16 11:38:55 -0700

    add bounding box for icons

--- src/component/ForceLayoutChart/index.js
+++ src/component/ForceLayoutChart/index.js
@@ -8,16 +8,29 @@ let iconNodes = []
 function ForceLayoutChart() {
   const { state } = useContext(StoreContext)
   const svgGroupRef = React.useRef(null)
+  const bounds = {
+    top: -20,
+    right: 600,
+    bottom: 250,
+    left: -40,
+  }
   function ticked() {
     for (let i = 0; i < iconNodes.length; i += 1) {
-      svgGroupRef.current.children[i].style.transform = `translate(${iconNodes[i].x}px, ${iconNodes[i].y}px)`
+      if (
+        iconNodes[i].x < bounds.right
+        && iconNodes[i].x > bounds.left
+        && iconNodes[i].y < bounds.bottom
+        && iconNodes[i].y > bounds.top
+      ) {
+        svgGroupRef.current.children[i].style.transform = `translate(${iconNodes[i].x}px, ${iconNodes[i].y}px)`
+      }
     }
   }
   useEffect(() => {
     iconNodes = [...state.iconNodes]
     const simulation = forceSimulation(iconNodes)
       .force('charge', forceManyBody())
-      .force('center', forceCenter(150, 75))
+      .force('center', forceCenter(0, 75))
     simulation.on('tick', () => ticked())
   }, [])
   const iconStyle = {
