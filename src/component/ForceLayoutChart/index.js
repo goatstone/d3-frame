8db40506e0cc475ddda44be5b449f9c39e7f5a61
Jose Collas <joseherminiocollas@goatstonc.com>
2019-07-17 10:56:27 -0700

    refactor drawing of view

--- src/component/ForceLayoutChart/index.js
+++ src/component/ForceLayoutChart/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import {
   forceSimulation,
   forceManyBody,
@@ -8,42 +8,23 @@ import {
 import { StoreContext } from '../../StoreContext'
 import getStyle, { styleTypes } from '../../get-style'
 
-let iconNodes = []
-const iconMax = Math.floor(50 * Math.random()) + 150
 function ForceLayoutChart() {
   const { state } = useContext(StoreContext)
+  const iconNodes = state.iconNodes.slice(0, 100)
+  const [iNState, setINState] = useState([{ name: 'aaa' }])
   const svgGroupRef = React.useRef(null)
-  const bounds = {
-    top: 50,
-    right: 450,
-    bottom: 600,
-    left: -20,
+  function tick() {
+    /** trigger the redraw */
+    setINState([{ name: `${iconNodes[0].x}px` }])
   }
-  function ticked() {
-    for (let i = 0; i < iconNodes.length; i += 1) {
-      if (
-        iconNodes[i].x < bounds.right
-        && iconNodes[i].x > bounds.left
-        && iconNodes[i].y < bounds.bottom
-        && iconNodes[i].y > bounds.top
-        && svgGroupRef.current
-      ) {
-        // this Ref may disapper!
-        svgGroupRef.current.children[i].style.transform = `
-        translate(${iconNodes[i].x}px, ${iconNodes[i].y}px)
-        `
-      }
-    }
-  }
-  iconNodes = state.iconNodes.slice(0, iconMax)
   useEffect(() => {
-    if (!iconNodes[0].x) {
-      const simulation = forceSimulation(iconNodes)
-        .force('charge', forceManyBody().strength(10))
-        .force('center', forceCenter(225, 250))
-        .force('collision', forceCollide().radius(15))
-      simulation.on('tick', () => ticked())
-    }
+    setINState([{ name: '22222' }])
+    const simulation = forceSimulation(iconNodes)
+    simulation
+      .force('charge', forceManyBody().strength(10))
+      .force('center', forceCenter(225, 250))
+      .force('collision', forceCollide().radius(15))
+    simulation.on('tick', () => tick())
   }, [])
   const iconStyle = {
     fontSize: '25px',
@@ -51,32 +32,36 @@ function ForceLayoutChart() {
     stroke: 'gray',
   }
   return (
-    <svg
-      className="main"
-      width="500"
-      height="500"
-    >
-      <rect
-        fill="white"
+    <div>
+      <svg
+        className="main"
         width="500"
         height="500"
-      />
-      <g ref={svgGroupRef} className={getStyle(state.theme, styleTypes.GENERIC).main()}>
-        {iconNodes.slice(0, iconMax).map(n => {
-          return (
-            <text
-              x={(n.x) ? n.x : 0}
-              y={(n.y) ? n.y : 0}
-              key={`${Math.random()}${n.name}`}
-              style={iconStyle}
-              className="material-icons"
-            >
-              {n.name}
-            </text>
-          )
-        })}
-      </g>
-    </svg>
+      >
+        /** trigger the redraw */
+        {iNState[0].name}
+        <rect
+          fill="white"
+          width="500"
+          height="500"
+        />
+        <g ref={svgGroupRef} className={getStyle(state.theme, styleTypes.GENERIC).main()}>
+          {iconNodes.map(n => {
+            return (
+              <text
+                x={(n.x)}
+                y={(n.y)}
+                key={`${Math.random()}${n.name}`}
+                style={iconStyle}
+                className="material-icons"
+              >
+                {n.name}
+              </text>
+            )
+          })}
+        </g>
+      </svg>
+    </div>
   )
 }
 export default ForceLayoutChart
