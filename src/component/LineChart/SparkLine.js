c7a71e1180e95f1c2742734747888b7d51aff6a4
Jose Collas <joseherminiocollas@goatstonc.com>
2019-08-25 09:18:41 -0700

    fix line chart fill

--- src/component/LineChart/SparkLine.js
+++ src/component/LineChart/SparkLine.js
@@ -5,12 +5,15 @@ const SparkLine = ({ data, xScale, yScale }) => {
   const sparkLine = d3.line()
     .x(d => xScale(new Date(d.day).setHours(0, 0, 0, 0)))
     .y(d => yScale(d.quality))
-
   const linePath = sparkLine(data)
 
   return (
     <g>
-      <path className="sparkline" d={linePath} />
+      <path
+        className="sparkline"
+        d={linePath}
+        fill="transparent"
+      />
     </g>
   )
 }
