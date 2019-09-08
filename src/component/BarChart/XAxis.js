6b451f478f144d73290818f5da3692e43fa234c2
Jose Collas <joseherminiocollas@goatstonc.com>
2019-09-08 15:42:40 -0700

    add scale to top level component in BarChart

--- src/component/BarChart/XAxis.js
+++ src/component/BarChart/XAxis.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import * as d3 from 'd3'
-import xScaleBarChart from './x-scale-bar-chart'
 
-const XAxis = ({ data, cssClasses }) => {
-  const width = parseInt(cssClasses.getRule('chartXAxis').prop('width'), 10)
+const XAxis = ({ cssClasses, xScale }) => {
   const xAxisD3Node = d3.axisBottom()
-    .scale(xScaleBarChart(data.map(d => d[0]), [0, width]))
-    .ticks(data.length / 2)
+    .scale(xScale)
 
   return (
     <g
--- src/component/BarChart/index.js
+++ src/component/BarChart/index.js
@@ -5,38 +5,39 @@ import XAxis from './XAxis'
 import YAxis from './YAxis'
 import { StoreContext } from '../../StoreContext'
 import { ThemeContext } from '../../theme/ThemeContext'
+import xScaleBarChart from './x-scale-bar-chart'
 
 const BarChart = () => {
   const { cssSheet } = useContext(ThemeContext)
+  const { state } = useContext(StoreContext)
+  // reuse this scale, pass it down to children
+  const xScale = xScaleBarChart(
+    state.data.bar.map(d => d[0]),
+    [0, state.chartSize.w],
+  )
   return (
-    <StoreContext.Consumer>
-      {
-        ({ state }) => (
-          <React.Fragment>
-            <ChartFrame
-              cssClasses={cssSheet}
-            >
-              <XAxis
-                data={state.data.bar}
-                cssClasses={cssSheet}
-              />
-              <YAxis
-                data={state.data.bar}
-                cssClasses={cssSheet}
-              />
-              <Bars
-                data={state.data.bar}
-                cssClasses={cssSheet}
-                chartSize={state.chartSize}
-              />
-              <text className={cssSheet.classes.chartText}>
-                The Frequency of Letters in the English Language
-              </text>
-            </ChartFrame>
-          </React.Fragment>
-        )
-      }
-    </StoreContext.Consumer>
+    <React.Fragment>
+      <ChartFrame
+        cssClasses={cssSheet}
+      >
+        <XAxis
+          cssClasses={cssSheet}
+          xScale={xScale}
+        />
+        <YAxis
+          data={state.data.bar}
+          cssClasses={cssSheet}
+        />
+        <Bars
+          data={state.data.bar}
+          cssClasses={cssSheet}
+          chartSize={state.chartSize}
+        />
+        <text className={cssSheet.classes.chartText}>
+          The Frequency of Letters in the English Language
+        </text>
+      </ChartFrame>
+    </React.Fragment>
   )
 }
 
