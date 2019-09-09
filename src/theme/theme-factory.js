b91337dfc47d837dcd5e9f9f0503a1adfe1d2568
Jose Collas <joseherminiocollas@goatstonc.com>
2019-09-09 08:21:43 -0700

    refactor theme WIP

--- src/theme/theme-factory.js
+++ src/theme/theme-factory.js
@@ -1,9 +1,9 @@
-import {
-  interpolateBlues,
-  interpolateGreens,
-  interpolateGreys,
-  interpolateReds,
-} from 'd3'
+// import {
+//   interpolateBlues,
+//   interpolateGreens,
+//   interpolateGreys,
+//   interpolateReds,
+// } from 'd3'
 import chartStyle from './chart-style'
 import controlStyle from './control-style'
 
@@ -13,13 +13,13 @@ const menuOptions = [
   { label: 'Green', color: 'green', keyValue: 'greenStyle' },
   { label: 'Gray', color: 'gray', keyValue: 'grayStyle' },
 ]
-const interpolators = {
-  defaultStyle: interpolateGreys,
-  grayStyle: interpolateGreys,
-  redStyle: interpolateReds,
-  greenStyle: interpolateGreens,
-  blueStyle: interpolateBlues,
-}
+// const interpolators = {
+//   defaultStyle: interpolateGreys,
+//   grayStyle: interpolateGreys,
+//   redStyle: interpolateReds,
+//   greenStyle: interpolateGreens,
+//   blueStyle: interpolateBlues,
+// }
 const defaultStyle = {
   mainContainer: {
     stroke: 'white',
@@ -37,26 +37,54 @@ const defaultStyle = {
 }
 Object.assign(defaultStyle, chartStyle, controlStyle)
 
-function colorize(interpolator) {
-  defaultStyle.mainContainer.background = interpolator(0.2)
-  defaultStyle.mainContainer.fill = interpolator(0.5)
-  defaultStyle.mainContainer.stroke = interpolator(0.7)
-  defaultStyle.chartFrame.fill = interpolator(0.3)
-  defaultStyle.chartFrame.stroke = interpolator(0.8)
-  defaultStyle.chartYAxis.stroke = interpolator(1)
-  defaultStyle.chartXAxis.stroke = interpolator(1)
-  defaultStyle.chartBars.fill = interpolator(0.7)
-  defaultStyle.chartBars.stroke = interpolator(0.8)
-  defaultStyle.chartIconForce.stroke = interpolator(0.3)
-  defaultStyle.chartIconForce.fill = interpolator(0.9)
-  defaultStyle.chartPies.fill = interpolator(0.2)
-  defaultStyle.chartPies.stroke = interpolator(0.8)
-  defaultStyle.chartPiesText.fill = interpolator(1)
+// function colorize(interpolator) {
+//   defaultStyle.mainContainer.background = interpolator(0.2)
+//   defaultStyle.mainContainer.fill = interpolator(0.5)
+//   defaultStyle.mainContainer.stroke = interpolator(0.7)
+//   defaultStyle.chartFrame.fill = interpolator(0.3)
+//   defaultStyle.chartFrame.stroke = interpolator(0.8)
+//   defaultStyle.chartYAxis.stroke = interpolator(1)
+//   defaultStyle.chartXAxis.stroke = interpolator(1)
+//   defaultStyle.chartBars.fill = interpolator(0.7)
+//   defaultStyle.chartBars.stroke = interpolator(0.8)
+//   defaultStyle.chartIconForce.stroke = interpolator(0.3)
+//   defaultStyle.chartIconForce.fill = interpolator(0.9)
+//   defaultStyle.chartPies.fill = interpolator(0.2)
+//   defaultStyle.chartPies.stroke = interpolator(0.8)
+//   defaultStyle.chartPiesText.fill = interpolator(1)
+//   const newStyleObject = JSON.parse(JSON.stringify(defaultStyle))
+//   return newStyleObject
+// }
+const styles = {
+  red: { label: 'Red' },
+  gray: { label: 'Gray' },
+  green: { label: 'Green' },
+  blue: { label: 'Blue' },
+}
+console.log(styles)
+const iVs = [
+  {
+    styleName: 'mainContainer',
+    styleProps: [
+      ['fill', 0],
+    ],
+  },
+]
+// :string : styleObject
+function themeFactory(themeKey = 'default') {
+  if (themeKey === 'default') return defaultStyle
   const newStyleObject = JSON.parse(JSON.stringify(defaultStyle))
+  for (let i = 0; i < iVs.length; i += 1) {
+    for (let j = 0; j < iVs[i].styleProps.length; j += 1) {
+      // console.log('XXXXXXXXXXXXXXXXXX', iVs[i].styleName)
+      newStyleObject[iVs[i].styleName][iVs[i].styleProps[j][0]] = 'red'
+    }
+  }
+  console.log('xx', newStyleObject.mainContainer)
   return newStyleObject
 }
-function themeFactory(themeName = 'defaultStyle') {
-  return colorize(interpolators[themeName])
-}
+// function themeFactory(themeName = 'defaultStyle') {
+//   return colorize(interpolators[themeName])
+// }
 export { menuOptions }
 export default themeFactory
