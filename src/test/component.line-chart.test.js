8548d539ee30042d922474ad4be7155253b13e39
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2019-10-03 16:32:22 -0700

    update tests

--- src/test/component.line-chart.test.js
+++ src/test/component.line-chart.test.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { StoreProvider } from '../StoreContext'
+import { ThemeProvider } from '../theme/ThemeContext'
 import LineChart from '../component/LineChart'
-import data from '../data'
-import config from '../config'
+
+jest.mock("jss", () => {
+  return {
+    setup: () => 1,
+    createStyleSheet: () => {
+      return {
+        attach: () => { },
+        classes: {mainContainer: {}}
+      }
+    },
+  }
+})
 
 describe('Line Chart', () => {
   it(' should  mount', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<LineChart
-      data={data.line}
-      config={config.chart}
-    />, div)
+    ReactDOM.render(
+      <ThemeProvider>
+        <StoreProvider>
+          <LineChart />
+        </StoreProvider>
+      </ThemeProvider>
+      , div)
     ReactDOM.unmountComponentAtNode(div)
   })
 })
