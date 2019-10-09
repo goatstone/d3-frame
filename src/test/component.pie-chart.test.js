fec73c494997d7a67a203487d5388c64d48f3f32
Jose Collas <joseherminiocollas@goatstonc.com>
2019-10-09 09:32:19 -0700

    update pie chart test with context

--- src/test/component.pie-chart.test.js
+++ src/test/component.pie-chart.test.js
@@ -1,12 +1,37 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import PieChart from '../component/PieChart'
 import data from '../data'
+import { ThemeContext } from '../theme/ThemeContext'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+jest.mock('../StoreContext', () => {
+  return {
+    StoreContext: {
+      Consumer: props => {
+        return props.children({
+          state: {
+            data: { bar: [] },
+            chartSize: { w: 0, h: 0 },
+          },
+        })
+      },
+    },
+  }
+})
 
 describe('Pie Chart', () => {
   it(' should  mount', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<PieChart data={data.bar} />, div)
-    ReactDOM.unmountComponentAtNode(div)
+    const wrapper = Enzyme.mount(
+      <ThemeContext.Provider value={{
+        cssSheet: { classes: {} },
+      }}
+      >
+        <PieChart data={data.bar} />
+      </ThemeContext.Provider>,
+    )
+    expect(wrapper.find('svg').length).toBe(1)
   })
 })
